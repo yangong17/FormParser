bf435d4ebdb878603f8b36355edbc230ac0f4ac6
yangong17 <yzong17@gmail.com>
2023-09-13 22:52:11 -0700

    updated textract

--- helpers.py
+++ helpers.py
@@ -1,35 +1,49 @@
-import boto3
 import csv
-from io import StringIO
+import io
+import os
+import boto3
+import time
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'pdf'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-import boto3
-import time
-
 def process_with_textract(file_path):
-    # Initialize the Textract client
-    client = boto3.client('textract', region_name='us-west-1')  # Make sure to replace 'your_region_name' with the appropriate AWS region.
+    # S3 bucket name
+    bucket_name = 'formparser'
+    
+    # Get just the filename for S3
+    s3_object_name = os.path.basename(file_path)
+    
+    # Initialize the S3 and Textract clients
+    s3 = boto3.client('s3', region_name='us-west-1')
+    textract = boto3.client('textract', region_name='us-west-1')
 
-    # Start the text detection process
+    # Upload the file to the S3 bucket
     with open(file_path, 'rb') as file:
-        start_response = client.start_document_text_detection(Document={'Bytes': file.read()})
+        s3.upload_fileobj(file, bucket_name, file_path)
 
-    job_id = start_response['JobId']
+    # Start the text detection process using the S3 object
+    response = textract.start_document_text_detection(
+        DocumentLocation={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': file_path
+            }
+        }
+    )
 
-    # Initially set the status to indicate that the job is in progress
+    job_id = response['JobId']
     status = 'IN_PROGRESS'
 
     # Poll the results of the job until it's no longer in progress
     while status == 'IN_PROGRESS':
-        time.sleep(5)  # Delay for 5 seconds before checking the job status again
-        response = client.get_document_text_detection(JobId=job_id)
+        time.sleep(5)  # Delay for 5 seconds
+        response = textract.get_document_text_detection(JobId=job_id)
         status = response['JobStatus']
 
-    # Once out of the loop, the job should be either 'SUCCEEDED', 'FAILED', or 'PARTIAL_SUCCESS'
+    # Check if the job failed
     if status == 'FAILED':
         print(f"Text detection failed with reason: {response['StatusMessage']}")
         return {}
@@ -47,14 +61,30 @@ def process_with_textract(file_path):
                         value = block['Text']
                         extracted_data[key] = value
 
-    return extracted_data
+    # Convert the extracted data to CSV format
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow(['Key', 'Value'])
+    for key, value in extracted_data.items():
+        writer.writerow([key, value])
 
+    # Get the content of the StringIO object
+    csv_content = output.getvalue()
 
+    # Save the CSV content to a file
+    with open('output.csv', 'w') as csv_file:
+        csv_file.write(csv_content)
 
-def convert_to_csv(data):
-    """Convert a list of dictionaries to a CSV string."""
-    output = StringIO()
-    writer = csv.DictWriter(output, fieldnames=data[0].keys())
-    writer.writeheader()
-    writer.writerows(data)
-    return output.getvalue()
+    delete_from_s3(bucket_name, s3_object_name)
+
+def delete_from_s3(bucket_name, object_key):
+    s3_client = boto3.client('s3', region_name='us-west-1')  # Adjust the region_name if necessary
+    s3_client.delete_object(Bucket=bucket_name, Key=object_key)
+
+#def convert_to_csv(data):
+ #   """Convert a list of dictionaries to a CSV string."""
+  #  output = StringIO()
+   # writer = csv.DictWriter(output, fieldnames=data[0].keys())
+   # writer.writeheader()
+   # writer.writerows(data)
+   # return output.getvalue()
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-from helpers import allowed_file, process_with_textract, convert_to_csv
+from helpers import allowed_file, process_with_textract, delete_from_s3
 
 
-process_with_textract("C:\\Users\\yong\\Documents\\Yangs Stuff\\Code\\ProjectsPersonal\\FormParser\\form_templates\\EmployeeInfo1.pdf")
+# process_with_textract("C:\\Users\\yong\\Documents\\Yangs Stuff\\Code\\ProjectsPersonal\\FormParser\\form_templates\\EmployeeInfo1.pdf")
+
+csv_content = process_with_textract("F:\\Coding Projects\\FormParser\\form_templates\\EmployeeInfo1.pdf")
+
 
 
 
