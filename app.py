0cbf22613260bcef6e8e37f1244c41b9ffefdc00
yango94 <yzong17@gmail.com>
2023-09-15 16:47:46 -0700

    updated formatting, working on databasing

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session, send_from_directory, url_for, jsonify
 from flask_session import Session
 from trp import Document
+from helpers import clean_text, setup_database, insert_data_into_db
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+app.debug = True
 
 # Configurations
 UPLOAD_FOLDER = 'uploads'
@@ -36,11 +38,40 @@ textract = boto3.client('textract',
 bucket_name = 'formparser'
 
 # -------------
+# Other Configs
 
+job_data = {} # in-memory storage for the job information
+
+# -------------
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+
+@app.route('/jobinformation', methods=['GET', 'POST'])
+def job_information():
+    if request.method == 'POST':
+        job_data['jobTitle'] = request.form['jobTitle']
+        job_data['yearsOfExperience'] = request.form['yearsOfExperience']
+        job_data['jobLocation'] = request.form['jobLocation']
+        job_data['salary'] = request.form['salary']
+        job_data['keyWords'] = clean_text(request.form['keyWords']).split(' ')
+        
+        infomsg = "Info Successfully Submitted"
+
+        print(job_data['jobTitle'])
+        print(job_data['yearsOfExperience'])
+        print(job_data['jobLocation'])
+        print(job_data['salary'])
+        print(job_data['keyWords'])
+        
+        return render_template("index.html", infomsg=infomsg, job_data=job_data)
+    else:
+        infomsg = "Failed to Submit Info"
+        return render_template("index.html", infomsg=infomsg, job_data=job_data)
+
+
 
 @app.route('/upload', methods=["POST"])
 def upload():
@@ -60,15 +91,49 @@ def upload():
                 Filename=full_path,  # Use the full path here
                 Key=s3_filename
             )
-            msg = "File Successfully Uploaded"
-            return render_template("index.html", msg=msg)
+            uploadmsg = "File Successfully Uploaded"
+            return render_template("index.html", uploadmsg=uploadmsg)
         else:
-            msg = "Failed to Upload File"
-            return render_template("index.html", msg=msg)
+            uploadmsg = "Failed to Upload File"
+            return render_template("index.html", uploadmsg=uploadmsg)
     
 
 @app.route("/execute", methods=["POST"])
 def execute():
+
+    # Config:
+    # List of keys to extract from the document and their data type
+    keys_to_extract = {
+        "First Name": "TEXT",
+        "Last Name": "TEXT",
+        "M.I": "TEXT",
+        "Date": "DATE",
+        "Street Address": "TEXT",
+        "Apartment/Unit#": "TEXT",
+        "City": "TEXT",
+        "State": "TEXT",
+        "Zip Code": "INTEGER",
+        "Phone": "TEXT",
+        "E-mail Address": "TEXT",
+        "Job(s) You're Applying For": "TEXT",
+        "Years of Experience": "INTEGER",
+        "Desired Salary/Rate": "REAL",
+        "Earliest Date Available": "DATE",
+        "Relevant Skills": "TEXT"
+    }
+
+
+    # Check if the database exists
+    if not os.path.exists('applications.db'):
+        setup_database(keys_to_extract)
+    else:
+        # If it exists, delete all data from the ApplicationData table
+        conn = sqlite3.connect('applications.db')
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM ApplicationData")
+        conn.commit()
+        conn.close()
+
     # Fetch the most recently uploaded file from S3 bucket
     objs = s3.list_objects_v2(Bucket=bucket_name)['Contents']
     objs.sort(key=lambda e: e['LastModified'], reverse=True)
@@ -80,63 +145,37 @@ def execute():
         FeatureTypes=["FORMS"]
     )
     doc = Document(response)
-    text = []
-    for page in doc.pages:
-
-        key = "First Name"
-        field = page.form.getFieldByKey(key)
-        if(field):
-            print("Key: {}, Value: {}".format(field.key, field.value))
-
-        field_amt = page.form.searchFieldsByKey("First Name")
-        for f in field_amt:
-            print("Key: {}, Value: {}".format(f.key, f.value))
-            text.append(f.key)
-            text.append(f.value)
-
-        key = "Last Name"
-        field = page.form.getFieldByKey(key)
-        if(field):
-            print("Key: {}, Value: {}".format(field.key, field.value))
-
-        field_amt = page.form.searchFieldsByKey("Last Name")
-        for f in field_amt:
-            print("Key: {}, Value: {}".format(f.key, f.value))
-            text.append(f.key)
-            text.append(f.value)
-
-        return render_template("index.html", text=text)
-
     
-    #doc = Document(response)
-    #text = []
-    #for page in doc.pages:
+    # Function to process and extract data from the document based on a given key
+    def process_field(page, key_name):
+        field = page.form.getFieldByKey(key_name)
 
-     #   key = "First Name"
-        #field = page.form.getFieldByKey(key)
-        #if(field):
-            #print("Key: {}, Value: {}".format(field.key, field.value))
-#
-        #key = "Last Name"
-        #field = page.form.getFieldByKey(key)
-        #if(field):
-            #print("Key: {}, Value: {}".format(field.key, field.value))
-#
-        #key = "M.I"
-        #field = page.form.getFieldByKey(key)
-        #if(field):
-            #print("Key: {}, Value: {}".format(field.key, field.value))
-#
-        #key = "Date"
-        #field = page.form.getFieldByKey(key)
-        #if(field):
-            #print("Key: {}, Value: {}".format(field.key, field.value))
-#
-    #return render_template("index.html")
+        data = {}  # Dictionary to store key-value pairs for database insertion
 
-        
+        if field and field.key in keys_to_extract.keys():
+            print("Key: {}, Value: {}".format(field.key, field.value))
+            # Store the key-value pair in the dictionary
+            data[field.key.replace(" ", "_").lower()] = field.value
 
+        field_amt = page.form.searchFieldsByKey(key_name)
+        for f in field_amt:
+            if f.key in keys_to_extract.keys():
+                print("Key: {}, Value: {}".format(f.key, f.value))
+                # Store the key-value pair in the dictionary
+                data[f.key.replace(" ", "_").lower()] = f.value
 
-# @app.route('/download/<filename>')
-# def download(filename):
-#    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+        return data
+
+    # Accumulate data from all keys
+    accumulated_data = {}  
+
+    for page in doc.pages:
+        for key in keys_to_extract.keys():
+            data_for_key = process_field(page, key)
+            accumulated_data.update(data_for_key)
+
+    # Insert the accumulated data into the database
+    insert_data_into_db(accumulated_data)
+
+    executemsg = "Data successfully stored in the database!"
+    return render_template("index.html", executemsg=executemsg)
--- helpers.py
+++ helpers.py
@@ -3,88 +3,74 @@ import io
 import os
 import boto3
 import time
+import string
+import sqlite3
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'pdf'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# -------
 
-def process_with_textract(file_path):
-    # S3 bucket name
-    bucket_name = 'formparser'
+def clean_text(text):
     
-    # Get just the filename for S3
-    s3_object_name = os.path.basename(file_path)
-    
-    # Initialize the S3 and Textract clients
-    s3 = boto3.client('s3', region_name='us-west-1')
-    textract = boto3.client('textract', region_name='us-west-1')
+    # List of stop words to remove
+    stop_words = ["and", "to", "the", "of", "in", "on", "with", "for", "as", "by", "an", "a", "is", "it", "at", "or"]
 
-    # Upload the file to the S3 bucket
-    with open(file_path, 'rb') as file:
-        s3.upload_fileobj(file, bucket_name, file_path)
+    # Remove unwanted characters and punctuation
+    text = text.replace("\r", " ").replace("\n", " ")
+    text = ''.join([char for char in text if char not in string.punctuation])
 
-    # Start the text detection process using the S3 object
-    response = textract.start_document_text_detection(
-        DocumentLocation={
-            'S3Object': {
-                'Bucket': bucket_name,
-                'Name': file_path
-            }
-        }
-    )
+    # Tokenize the string
+    words = text.split()
 
-    job_id = response['JobId']
-    status = 'IN_PROGRESS'
+    # Remove stop words
+    cleaned_words = [word for word in words if word.lower() not in stop_words]
 
-    # Poll the results of the job until it's no longer in progress
-    while status == 'IN_PROGRESS':
-        time.sleep(5)  # Delay for 5 seconds
-        response = textract.get_document_text_detection(JobId=job_id)
-        status = response['JobStatus']
+    return ' '.join(cleaned_words)
 
-    # Check if the job failed
-    if status == 'FAILED':
-        print(f"Text detection failed with reason: {response['StatusMessage']}")
-        return {}
+# -------
 
-    # Extract key-value pairs from the Textract response
-    extracted_data = {}
-    for item in response["Blocks"]:
-        if item["BlockType"] == "KEY_VALUE_SET":
-            if 'KEY' in item['EntityTypes']:
-                key = item['Text']
-                value_block_id = item['Relationships'][0]['Ids'][0]
-                
-                for block in response["Blocks"]:
-                    if block['Id'] == value_block_id:
-                        value = block['Text']
-                        extracted_data[key] = value
+def setup_database(keys_to_extract):
+    try:
+        conn = sqlite3.connect('applications.db')
+        cursor = conn.cursor()
+        
+        # Generating the column names and types dynamically
+        columns = ", ".join([f"{key.replace(' ', '_').lower()} {value}" for key, value in keys_to_extract.items()])
+        
+        # SQL command
+        sql = f'''
+        CREATE TABLE IF NOT EXISTS ApplicationData (
+            id INTEGER PRIMARY KEY,
+            {columns}
+        )
+        '''
+        
+        cursor.execute(sql)
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error occurred: {e}")
 
-    # Convert the extracted data to CSV format
-    output = io.StringIO()
-    writer = csv.writer(output)
-    writer.writerow(['Key', 'Value'])
-    for key, value in extracted_data.items():
-        writer.writerow([key, value])
+# -------
 
-    # Get the content of the StringIO object
-    csv_content = output.getvalue()
+def insert_data_into_db(data):
+    conn = sqlite3.connect('applications.db')
+    cursor = conn.cursor()
 
-    # Save the CSV content to a file
-    with open('output.csv', 'w') as csv_file:
-        csv_file.write(csv_content)
+    # Convert keys to valid named placeholders
+    formatted_keys = [key.replace(" ", "_").lower() for key in data.keys()]
 
-    delete_from_s3(bucket_name, s3_object_name)
+    columns = ', '.join(formatted_keys)
+    placeholders = ':' + ', :'.join(formatted_keys)
 
-def delete_from_s3(bucket_name, object_key):
-    s3_client = boto3.client('s3', region_name='us-west-1')  # Adjust the region_name if necessary
-    s3_client.delete_object(Bucket=bucket_name, Key=object_key)
+    sql = f"INSERT INTO ApplicationData ({columns}) VALUES ({placeholders})"
 
-#def convert_to_csv(data):
- #   """Convert a list of dictionaries to a CSV string."""
-  #  output = StringIO()
-   # writer = csv.DictWriter(output, fieldnames=data[0].keys())
-   # writer.writeheader()
-   # writer.writerows(data)
-   # return output.getvalue()
+    # Create a new dictionary with formatted keys to match placeholders
+    formatted_data = {key.replace(" ", "_").lower(): value for key, value in data.items()}
+
+    cursor.execute(sql, formatted_data)
+    conn.commit()
+    conn.close()
+
